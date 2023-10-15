af82844d61e2e63ab398bca5c8919684500f84d3
Hyun-Soon <s7514036@naver.com>
2023-10-15 21:12:15 +0900

    modify page_content fontsize and background color

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Makefile",
-        "excerpt":"Make란? “의존성 관리” 기능을 갖춘 빌드 도구. 의존성 관리 빌드 과정에서 의존성에 따른 빌드 순서는 무척 중요하다. a.c로 a.o를 만들고, a.o로 a.out을 만드는 상황을 가정하자. 여기서 a.out은 a.o에, a.o는 a.c에 의존성이 있다고 할 수 있다. 만약 의존성 그래프가 아래와 같이 복잡해지면 어떨까? test를 수행하기 위해 순서를 잘 지키면서 과정을 따라가야...","categories": ["42Seoul"],
+        "excerpt":"Make란? “의존성 관리” 기능을 갖춘 빌드 도구. 의존성 빌드 과정에서 의존성에 따른 빌드 순서는 무척 중요하다. a.c로 a.o를 만들고, a.o로 a.out을 만드는 상황을 가정하자. 여기서 a.out은 a.o에, a.o는 a.c에 의존성이 있다고 할 수 있다. 만약 의존성 그래프가 아래와 같이 복잡해지면 어떨까? test를 수행하기 위해 순서를 잘 지키면서 과정을 따라가야 한다....","categories": ["42Seoul"],
         "tags": ["42Seoul","Makefile"],
         "url": "/42seoul/test-copy-notion/",
         "teaser": null
