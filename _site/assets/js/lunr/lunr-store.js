baa9cecf5e8732590b815275bf71edf62df57d42
Hyun-Soon <s7514036@naver.com>
2023-10-19 22:17:26 +0900

    post libft

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,6 +5,18 @@ var store = [{
         "url": "/etc/Markdown/",
         "teaser": null
       },{
+        "title": "Test",
+        "excerpt":"abc      test    test tab  ","categories": [],
+        "tags": [],
+        "url": "/test/",
+        "teaser": null
+      },{
+        "title": "Libft",
+        "excerpt":"0. 과제 설명 libc에 있는 함수 및 앞으로의 문제를 푸는 데 유용한 함수들을 구현하고 library로 만든다. 1. Library 1-1. Library란? 라이브러리는 주로 소프트웨어를 개발할 때 컴퓨터 프로그램이 사용하는 비휘발성 자원의 모임이다. 비휘발성 자원의 예로는 미리 작성된 코드, 함수, 클래스, 자료형 등이 있다. 라이브러리는 재사용이 필요한 기능의 반복적인 코드 작성을 피하고, 언제든지 필요한 곳에서 호출하여...","categories": ["42Seoul"],
+        "tags": ["42Seoul","Libft","Archive","Library"],
+        "url": "/42seoul/Libft/",
+        "teaser": null
+      },{
         "title": "Makefile 문법 및 예시",
         "excerpt":"0. 들어가며 빌드를 쉽게 해주는 make와 Makefile에 대해 알아보자. 1. make란? \"의존성 관리”와 “증분 빌드” 기능을 갖춘 빌드 도구. 1-1. 의존성 관리 빌드 과정에서 의존성에 따른 빌드 순서는 무척 중요하다. 컴파일, 즉 a.c로 a.o를 만들고, a.o로 a.out을 만드는 상황을 가정하자. 여기서 a.out은 a.o에, a.o는 a.c에 의존성이 있다고 할 수 있다....","categories": ["42Seoul"],
         "tags": ["42Seoul","Linux","Make","Makefile"],
